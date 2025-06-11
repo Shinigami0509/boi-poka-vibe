f10f3e323440b7d4771152971fd397e7d9d70e62
Shinigami0509 <walidbin.kamal64@gmail.com>
2025-06-11 17:40:08 +0600

    done

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -8,21 +8,49 @@ import Book from '../Book/Book';
 const ListedBooks = () => {
 
     const [readList, setReadList] = useState([])
-    
+    const [sort, setSort] = useState('')
+
     //ideally we will directly get readlist from database
     const allBooks = useLoaderData()
-    useEffect(()=>{
+    useEffect(() => {
         const storedReadList = getStoredReadList()
-        const storedReadListInt = storedReadList.map(id=>parseInt(id))
-        
-        const readBooklist = allBooks.filter(book=>storedReadListInt.includes(book.bookId)) 
+        const storedReadListInt = storedReadList.map(id => parseInt(id))
+
+        const readBooklist = allBooks.filter(book => storedReadListInt.includes(book.bookId))
         setReadList(readBooklist)
 
-    },[])
+    }, [])
+
+    const handleSort = sortType => {
+        setSort(sortType)
+
+        if (sortType === 'No of Pages') {
+            const sortedReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages)
+            setReadList(sortedReadList)
+        }
+        if (sortType === 'Ratings') {
+            const sortedReadList = [...readList].sort((a, b) => a.rating - b.rating)
+            setReadList(sortedReadList)
+        }
+
+    }
 
     return (
         <div>
             <h3 className="text-3xl my-8">Listed Books</h3>
+
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn m-1">
+                    {
+                        sort ? `Sort By ${sort}` : 'Sort By'
+                    }
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li onClick={() => handleSort("Ratings")}><a>Ratings</a></li>
+                    <li onClick={() => handleSort("No of Pages")}><a>No of Pages</a></li>
+                </ul>
+            </div>
+
             <Tabs>
                 <TabList>
                     <Tab>Read List</Tab>
@@ -32,11 +60,11 @@ const ListedBooks = () => {
                 <TabPanel>
                     <h2 className="text-2xl">Books I Read: {readList.length}</h2>
                     {
-                        readList.map(book=><Book key={book.bookId} book={book}></Book>)
+                        readList.map(book => <Book key={book.bookId} book={book}></Book>)
                     }
                 </TabPanel>
                 <TabPanel>
-                     <h2 className="text-2xl">My Wish List</h2>
+                    <h2 className="text-2xl">My Wish List</h2>
                 </TabPanel>
             </Tabs>
         </div>
